76216dcddb8deb013a3111e5e8ee0930a8a57e1c
Healerdhan <healerhec@gmail.com>
2024-07-17 02:26:00 +0700

    front end

---
+++ public/js/app.js
@@ -0,0 +1,19 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   fetch('/api/category')
+//       .then(response => response.json())
+//       .then(data => {
+//           console.log(data);  // Tampilkan data yang diterima dari API untuk debugging
+//           const categoryTableBody = document.querySelector('#category-table tbody');
+//           data.data.forEach(category => {
+//               const row = document.createElement('tr');
+//               row.innerHTML = `
+//                   <td>${category.id}</td>
+//                   <td>${category.jurusan}</td>
+//                   <td>${category.created_at}</td>
+//                   <td>${category.updated_at}</td>
+//               `;
+//               categoryTableBody.appendChild(row);
+//           });
+//       })
+//       .catch(error => console.error('Error fetching data:', error));
+// });
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,8 @@
+<nav>
+  <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/categories">Categories</a></li>
+      <li><a href="/another-page">Another Page</a></li>
+      <!-- Tambahkan item navbar lain di sini -->
+  </ul>
+</nav>
