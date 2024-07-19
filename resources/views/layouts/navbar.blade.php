43ba6046bb6abb2fe2853d877a8b3cb20ada84ff
hyugarh <arifrarh17@gmail.com>
2024-07-19 21:00:18 +0700

    tes

--- resources/views/layouts/navbar.blade.php
+++
@@ -1,8 +0,0 @@
-<nav>
-  <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/categories">Categories</a></li>
-      <li><a href="/another-page">Another Page</a></li>
-      <!-- Tambahkan item navbar lain di sini -->
-  </ul>
-</nav>
